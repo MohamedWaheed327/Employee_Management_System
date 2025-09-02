c550b8556d1d0ec43d8289a17a50d01c8d6411d2
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2025-09-02 06:18:17 +0300

    Job model Created

---
+++ Employee_Management_System.EMS/Models/Job.cs
@@ -0,0 +1,10 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Management_System.EMS.Models;
+
+public class Job {
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+}
