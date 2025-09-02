0266fb48f99ef5dce74005f54f636bb48643b982
MWarith <mwarith25@gmail.com>
2025-09-02 20:37:59 +0300

    file formatted

--- Employee_Management_System.EMS/ViewModel/EmployeeListVm.cs
+++ Employee_Management_System.EMS/ViewModel/EmployeeListVm.cs
@@ -4,18 +4,18 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace Employee_Management_System.EMS.Models;
 
 public class EmployeeListVm {
-        public string? Search { get; set; }
-        public int? DepartmentId { get; set; }
-        public int? JobTitleId { get; set; }
+    public string? Search { get; set; }
+    public int? DepartmentId { get; set; }
+    public int? JobTitleId { get; set; }
 
-        public int Page { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
-        public int TotalCount { get; set; }
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public int TotalCount { get; set; }
 
-        public List<Employee> Items { get; set; } = new();
+    public List<Employee> Items { get; set; } = new();
 
-        public SelectList? Departments { get; set; }
-        public SelectList? JobTitles { get; set; }
-    }
+    public SelectList? Departments { get; set; }
+    public SelectList? JobTitles { get; set; }
+}
 
 
