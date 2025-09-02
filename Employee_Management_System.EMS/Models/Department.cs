94b167ec88db3efa61874377e1b1610bc1a7ff09
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2025-09-02 06:17:58 +0300

    Department Model Created

---
+++ Employee_Management_System.EMS/Models/Department.cs
@@ -0,0 +1,11 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Management_System.EMS.Models;
+
+public class Department {
+    [Key]
+    public int Id { get; set; }
+    // [AllowedValues("IT", "HR")]
+    public string Name { get; set; }
+}
