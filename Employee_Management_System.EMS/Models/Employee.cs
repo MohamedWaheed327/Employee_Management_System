dc5e024c67b76def4fe1cc02c7fa08435676152d
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2025-09-02 06:06:49 +0300

    Employee Model Created

---
+++ Employee_Management_System.EMS/Models/Employee.cs
@@ -0,0 +1,15 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Management_System.EMS.Models;
+
+public class Employee {
+    [Key]
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime HireDate { get; set; }
+    public decimal Salary { get; set; }
+    public string? ProfileImagePath { get; set; }
+}
