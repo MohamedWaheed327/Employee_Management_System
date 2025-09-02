92fe1d6b3c0c9446c3202d68ca441b070c53c9af
MohamedThaer <125297147+MohamedThaer@users.noreply.github.com>
2025-09-02 14:48:22 +0300

    DB con

--- Employee_Management_System.EMS/Program.cs
+++ Employee_Management_System.EMS/Program.cs
@@ -3,13 +3,17 @@
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<EMS_DBcontext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ITIConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+if (!app.Environment.IsDevelopment())
+{
+   app.UseExceptionHandler("/Home/Error");
+   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   app.UseHsts();
 }
 
 app.UseHttpsRedirection();//Mohammed Ramadan
