8dca9dfefc247fee62a6c38e5d459833cb0fad78
MWarith <mwarith25@gmail.com>
2025-09-02 15:23:19 +0300

    adding nullabillity

--- Employee_Management_System.EMS/Models/Employee.cs
+++ Employee_Management_System.EMS/Models/Employee.cs
@@ -8,14 +8,14 @@ public class Employee {
     [Key]
     // [System.ComponentModel.DataAnnotations.]
     public int Id { get; set; }
-    public string FullName { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
     public DateTime HireDate { get; set; }
     public decimal Salary { get; set; }
     public string? ProfileImagePath { get; set; }
 
-    public Department department { get; set; }
+    public Department? department { get; set; }
     [ForeignKey(nameof(department))]
     public int Department_id { get; set; }
 
--- Employee_Management_System.EMS/Models/Job.cs
+++ Employee_Management_System.EMS/Models/Job.cs
@@ -6,6 +6,6 @@ namespace Employee_Management_System.EMS.Models;
 public class Job {
     [Key]
     public int Id { get; set; }
-    public string Title { get; set; }
-    public List<Employee> employees { get; set; }
+    public string? Title { get; set; }
+    public List<Employee>? employees { get; set; }
 }
