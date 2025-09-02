43c5a66f2ee36446e55811f90b11334adca5a46f
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2025-09-02 10:09:36 +0300

    EMS_DBcontext created

---
+++ Employee_Management_System.EMS/Models/EMS_DBcontext.cs
@@ -0,0 +1,27 @@
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management_System.EMS.Models;
+
+public class EMS_DBcontext : DbContext {
+    DbSet<Employee> employees;
+    DbSet<Job> jobs;
+    DbSet<Department> departments;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=ITI_EMS; Trusted_Connection=True;Integrated Security=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<Employee>()
+        .HasOne(e => e.department)
+        .WithMany(d => d.employees)
+        .HasForeignKey(e => e.department);
+
+        modelBuilder.Entity<Employee>()
+        .HasOne(e => e.job)
+        .WithMany(d => d.employees)
+        .HasForeignKey(e => e.job);
+    }
+}
--- Employee_Management_System.EMS/Models/Employee.cs
+++ Employee_Management_System.EMS/Models/Employee.cs
@@ -1,10 +1,12 @@
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Employee_Management_System.EMS.Models;
 
 public class Employee {
     [Key]
+    // [System.ComponentModel.DataAnnotations.]
     public int Id { get; set; }
     public string FullName { get; set; }
     public string Email { get; set; }
@@ -12,6 +14,12 @@ public class Employee {
     public DateTime HireDate { get; set; }
     public decimal Salary { get; set; }
     public string? ProfileImagePath { get; set; }
+
+    public int Department_id { get; set; }
+    [ForeignKey(nameof(Department_id))]
     public Department department { get; set; }
+
+    public int Job_id { get; set; }
+    [ForeignKey(nameof(Job_id))]
     public Job job { get; set; }
 }
